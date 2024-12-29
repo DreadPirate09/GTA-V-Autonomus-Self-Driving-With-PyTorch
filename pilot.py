67f14e0cf80de8d06093ae30313ca2ed9930d170
DreadPirate09 <62398759+DreadPirate09@users.noreply.github.com>
2024-12-29 22:05:27 +0200

    Some changes to the Pilot

--- pilot.py
+++ pilot.py
@@ -10,14 +10,16 @@ class Pilot(object):
 
     def sendIt(self, steering, throttle, brake, speed):
         steering = steering*2.0 - 1.0
-        if speed < 30 and throttle < 0.65:
-            throttle = 0.85
-        if speed > 70:
+        if throttle < 0.25 and abs(steering) < 0.3:
+            throttle = 0.30
+        if speed < 30 and throttle < 0.65 and abs(steering) < 0.3:
+            throttle = 0.75
+        if speed > 60:
             throttle = 0.0
         # if throttle > self.max_throttle:
         #     throttle = self.max_throttle
 
-        steering = steering * 1.6
+        steering = steering * 1.9
         self.controller.set_value('AxisLx', steering) 
         self.controller.set_value('TriggerR', throttle) 
         self.controller.set_value('TriggerL', brake) 
