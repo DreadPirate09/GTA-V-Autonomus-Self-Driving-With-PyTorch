b6230b029212509a6b4c5bf4eec16855668df3a0
DreadPirate09 <62398759+DreadPirate09@users.noreply.github.com>
2024-12-30 21:20:12 +0200

    Use avg speed running the send function

--- pilot.py
+++ pilot.py
@@ -1,5 +1,8 @@
 import pyxinput
 import time
+import pandas as pd
+
+TRAIN_DATA_FILE = "data/data.csv"
 
 class Pilot(object):
 
@@ -7,6 +10,7 @@ class Pilot(object):
         self.name = name
         self.controller = pyxinput.vController(percent=True)
         self.max_throttle = max_throttle
+        self.avg_speed = self.getAvgSpeed()
 
     def sendIt(self, steering, throttle, brake, speed):
         steering = steering*2.0 - 1.0
@@ -14,8 +18,8 @@ class Pilot(object):
             throttle = 0.30
         if speed < 30 and throttle < 0.65 and abs(steering) < 0.3:
             throttle = 0.75
-        if speed > 60:
-            throttle = 0.0
+        if speed > self.avg_speed:
+            throttle = throttle / 3.0
         # if throttle > self.max_throttle:
         #     throttle = self.max_throttle
 
@@ -25,3 +29,9 @@ class Pilot(object):
         self.controller.set_value('TriggerL', brake) 
 
         print(f"Steering: {steering}, Throttle: {throttle}, Brake: {brake}")
+
+    def getAvgSpeed(self):
+        data = pd.read_csv(TRAIN_DATA_FILE)
+        avg_speed = sum([data.iloc[x,3] for x in range(len(data))])/len(data)
+
+        return avg_speed
